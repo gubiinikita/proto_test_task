30d53c1f0670dda3af61df5973c3d954c467f617
clear <clear@MacBook-Pro-Nik.local>
2023-03-07 15:42:55 +0200

    deleted

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const {baseDiceMax, baseDiceMin} = config;
 describe('Dices distribution', () => {
     const rollsNum = process.env.ROLLS_NUM || 10;
     const min = process.env.MIN || baseDiceMin;
-    const max = process.env.MAX || baseDiceMax``;
+    const max = process.env.MAX || baseDiceMax;
 
     it(`${min} dice distribution`, async () => {
         console.log(`Roll with parameters: number of rolls [${rollsNum}], count of dices [${min}]`);
